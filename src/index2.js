bab234aa1e530e1ce733c17132351ffcfa18e619
fd-2-snezhko-alex <desert_eagle666@!mail.ru>
2022-09-23 21:38:05 +0300

    3

---
+++ src/index2.js
@@ -0,0 +1,60 @@
+// const currency = {
+//     usd,
+//     euro,
+//     rub
+// }
+
+// let descriptor = Object.defineProperty(currency,"usd",{
+//     writable:false,
+//     enumerable:false,
+//     configurable:false,
+// });
+
+// descriptor.usd = "wertyn";
+// console.log(descriptor);
+
+// let salaries ={
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130,
+// }
+// let summ = 0;
+
+// for(const key in salaries) {
+//     summ += salaries[key];
+// }
+
+// console.log.summ
+
+// function someSummary(a,b) {
+//     alert(a+b)
+// }
+
+// someSummary(2,3)
+
+// const summary = (...params) => {
+//     let sum = 0;
+//     for (sum of params)
+//         sum+=sum1
+//     }
+
+//     return sum
+    
+//     console.log.length(fn(1,2,3,4,5));
+
+
+function min(a, b) {
+    if (a < b) {
+      return a;
+    } 
+    else {
+      return b;
+    }
+  }
+
+  const isPalindrom = (text) =>{
+      let text2 = '';
+      for(let i = text.length-1;i > = 0; i--) (
+
+      )
+  }
