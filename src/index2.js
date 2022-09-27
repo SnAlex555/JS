944c3a682da82bb5e61e32804d15399f6ac9b1c3
SnAlex555 <desert_eagle666@mail.ru>
2022-09-27 21:31:45 +0300

    sas

--- src/index2.js
+++ src/index2.js
@@ -43,18 +43,50 @@
 //     console.log.length(fn(1,2,3,4,5));
 
 
-function min(a, b) {
-    if (a < b) {
-      return a;
-    } 
-    else {
-      return b;
-    }
-  }
+// function min(a, b) {
+//     if (a < b) {
+//       return a;
+//     } 
+//     else {
+//       return b;
+//     }
+//   }
 
-  const isPalindrom = (text) =>{
-      let text2 = '';
-      for(let i = text.length-1;i > = 0; i--) (
+//   const isPalindrom = (text) =>{
+//       let text2 = '';
+//       for(let i = text.length-1;i > = 0; i--) (
 
-      )
-  }
+//       )
+//   }
+
+// function sumTo(n) {
+//   if (n == 1) return 1;
+//   return n + sumTo(n-1);
+// }
+
+// alert( sumTo(100) );
+
+// function sum(a) {
+//   return function(b) {
+//   return sum(a+b);
+//    };
+// };
+
+// (sum(1)(2));
+
+// const list = {
+//   a: "a",
+//   b: "b",
+//   next: {
+//     a: "a-1",
+//     b: "b-1",
+//     next: {
+//       a: "a-2",
+//       b: "b-2",
+//       next:null
+//     }
+//   }
+// }
+
+let string = 'aaa bbb ccc';
+console.log(string.slice(string,string + 3));
